fb0f2205e85ece11476a7ef4531a5af4b3f84391
anubhajoshi01 <anubhajoshi01@gmail.com>
2023-11-12 19:51:13 -0500

    add presenter and output boundary code

--- src/use_case/create_mood/MoodDataAccessInterface.java
+++ src/use_case/create_mood/MoodDataAccessInterface.java
@@ -2,13 +2,13 @@ package use_case.create_mood;
 
 import entity.Mood;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public interface MoodDataAccessInterface {
 
     public Map<String, Mood> getMoods();
+
+    public Set<String> getMoodNames();
 
     public void saveMood(String name, double acousticness, double danceability, double energy, double instrumentalness,
                          double liveness, double speechiness, double valence);
