7c2fa816db5fe9164f1699e215a33b11adbb2a2e
omina26 <omina.nematova@mail.utoronto.ca>
2023-11-25 12:04:57 -0500

    Basic implementation of Create Playlist. No functinality yet.

--- src/interface_adapter/create_playlist/CreatePlaylistPresenter.java
+++ src/interface_adapter/create_playlist/CreatePlaylistPresenter.java
@@ -1,4 +1,39 @@
 package interface_adapter.create_playlist;
 
-public class CreatePlaylistPresenter {
+import interface_adapter.ViewManagerModel;
+import interface_adapter.playlist_created.PlaylistCreatedState;
+import interface_adapter.playlist_created.PlaylistCreatedViewModel;
+import use_case.create_playlist.CreatePlaylistOutputBoundary;
+
+public class CreatePlaylistPresenter implements CreatePlaylistOutputBoundary {
+
+    private final CreatePlaylistViewModel createPlaylistViewModel;
+
+    private final PlaylistCreatedViewModel playlistCreatedViewModel;
+
+    private ViewManagerModel viewManagerModel;
+
+    public CreatePlaylistPresenter(CreatePlaylistViewModel createPlaylistViewModel, PlaylistCreatedViewModel playlistCreatedViewModel,
+                                   ViewManagerModel viewManagerModel) {
+        this.createPlaylistViewModel = createPlaylistViewModel;
+        this.playlistCreatedViewModel = playlistCreatedViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    //@Override
+    public void prepareSuccessView() {
+        System.out.println("Playlist successfully created");
+        PlaylistCreatedState playlistCreatedState = playlistCreatedViewModel.getState();
+        this.playlistCreatedViewModel.setState(playlistCreatedState);
+        this.playlistCreatedViewModel.firePropertyChanged();
+        this.viewManagerModel.setActiveView(playlistCreatedViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    //@Override
+    public void prepareFailView(String error) {
+        CreatePlaylistState createPlaylistState = createPlaylistViewModel.getState();
+        createPlaylistState.setSaveError(error);
+        createPlaylistViewModel.firePropertyChanged();
+    }
 }
--- src/interface_adapter/create_playlist/CreatePlaylistState.java
+++ src/interface_adapter/create_playlist/CreatePlaylistState.java
@@ -1,4 +1,18 @@
 package interface_adapter.create_playlist;
 
 public class CreatePlaylistState {
+    private String selectedMood;
+
+    public CreatePlaylistState(String defaultMessage) {
+        this.selectedMood = defaultMessage;
+    }
+
+    public String getSelectedMood() {return selectedMood;}
+
+    public void setSelectedMood(String selectedMood) {
+        this.selectedMood = selectedMood;
+    }
+
+    public void setSaveError(String error) {
+    }
 }
