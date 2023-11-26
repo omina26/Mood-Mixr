7f47b7cf8049201f46d0eec72ae41015931c8baa
Kelsey Chen <kels.chen@mail.utoronto.ca>
2023-11-26 16:45:36 -0500

    completed AnalyzePlaylistUseCaseFactory.java

--- src/view/AnalyzePlaylistView.java
+++ src/view/AnalyzePlaylistView.java
@@ -1,4 +1,19 @@
 package view;
 
-public class AnalyzePlaylistView {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class AnalyzePlaylistView extends JPanel implements ActionListener, PropertyChangeListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
 }
