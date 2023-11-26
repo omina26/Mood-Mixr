29cc40753034d353236c6474c9b33757cef31cd5
Kelsey Chen <kels.chen@mail.utoronto.ca>
2023-11-26 17:02:36 -0500

    updated AnalyzePlaylistPresenter

--- src/interface_adapter/analyze_playlist/AnalyzePlaylistPresenter.java
+++ src/interface_adapter/analyze_playlist/AnalyzePlaylistPresenter.java
@@ -2,8 +2,17 @@ package interface_adapter.analyze_playlist;
 
 import interface_adapter.ViewManagerModel;
 import use_case.analyze_playlist.AnalyzePlaylistOutputBoundary;
+import use_case.analyze_playlist.AnalyzePlaylistOutputData;
 
 public class AnalyzePlaylistPresenter implements AnalyzePlaylistOutputBoundary {
-    public AnalyzePlaylistPresenter(AnalyzePlaylistViewModel analyzePlaylistViewModel, ViewManagerModel viewManagerModel) {
+    private final AnalyzePlaylistViewModel analyzePlaylistViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public AnalyzePlaylistPresenter(AnalyzePlaylistViewModel analyzePlaylistViewModel,
+                                    ViewManagerModel viewManagerModel) {
+        this.analyzePlaylistViewModel = analyzePlaylistViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
     }
 }
