cfc3965e010300be495bf17e782637a3822cf381
Kelsey Chen <kels.chen@mail.utoronto.ca>
2023-11-26 17:44:51 -0500

    added Analyze Playlist button and view

--- src/interface_adapter/analyze_playlist/AnalyzePlaylistViewModel.java
+++ src/interface_adapter/analyze_playlist/AnalyzePlaylistViewModel.java
@@ -1,4 +1,37 @@
 package interface_adapter.analyze_playlist;
 
-public class AnalyzePlaylistViewModel {
+import interface_adapter.ViewModel;
+import use_case.analyze_playlist.AnalyzePlaylistOutputData;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class AnalyzePlaylistViewModel extends ViewModel {
+    public final String TITLE_LABLE = "Analyze Playlist View";
+
+    public static final String SELECT_PLAYLIST_LABLE = "Select Playlist";
+
+    private AnalyzePlaylistState state = new AnalyzePlaylistState(SELECT_PLAYLIST_LABLE);
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public AnalyzePlaylistViewModel(String viewName) {
+        super("Analyze Playlist");
+    }
+
+    public void setState (AnalyzePlaylistState state){
+        this.state = state;}
+
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public AnalyzePlaylistState getState(){
+        return state;
+    }
 }
