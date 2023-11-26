46aeb845c20138d066a0753d24c396e1bc51af51
Kelsey Chen <kels.chen@mail.utoronto.ca>
2023-11-26 16:54:34 -0500

    updated code for AnalyzePlaylistController

--- src/interface_adapter/analyze_playlist/AnalyzePlaylistController.java
+++ src/interface_adapter/analyze_playlist/AnalyzePlaylistController.java
@@ -3,6 +3,9 @@ package interface_adapter.analyze_playlist;
 import use_case.analyze_playlist.AnalyzePlaylistInputBoundary;
 
 public class AnalyzePlaylistController {
-    public AnalyzePlaylistController(AnalyzePlaylistInputBoundary analyzePlaylistInputBoundary) {
+    final AnalyzePlaylistInputBoundary analyzePlaylistInteractor;
+    public AnalyzePlaylistController(AnalyzePlaylistInputBoundary analyzePlaylistInteractor) {
+        this.analyzePlaylistInteractor = analyzePlaylistInteractor;
     }
+
 }
