c30bfc20af0ebee360505bd4f4b20261b6b4eb8d
Kelsey Chen <kels.chen@mail.utoronto.ca>
2023-11-27 20:52:29 -0500

    added headers in the cvs file for average mood data of the analyzed playlist

--- src/data_access/analyze_playlist/AnalyzePlaylistDataAccessObject.java
+++ src/data_access/analyze_playlist/AnalyzePlaylistDataAccessObject.java
@@ -3,8 +3,35 @@ package data_access.analyze_playlist;
 import use_case.analyze_playlist.AnalyzePlaylistDataAccessInterface;
 
 import java.io.File;
+import java.io.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-public class AnalyzePlaylistDataAccessObject extends AnalyzePlaylistDataAccessInterface {
+public class AnalyzePlaylistDataAccessObject implements AnalyzePlaylistDataAccessInterface {
     private File csvFile;
+    private final Map<String, Integer> headers = new LinkedHashMap<>();
+
+    public AnalyzePlaylistDataAccessObject(File csvFile) throws IOException {
+        this.csvFile = csvFile;
+        headers.put("playlist name", 0);
+        headers.put("average acousticness", 1);
+        headers.put("average danceability", 2);
+        headers.put("average energy", 3);
+        headers.put("average instrumentalness", 4);
+        headers.put("average liveness", 5);
+        headers.put("average speechiness", 6);
+        headers.put("average valance", 7);
+
+        File cvsFile;
+        if (cvsFile.length() == 0){
+            save();
+            return;
+        }
+
+
+    }
+
+    private void save() {
+    }
 
 }
--- src/use_case/analyze_playlist/AnalyzePlaylistInteractor.java
+++ src/use_case/analyze_playlist/AnalyzePlaylistInteractor.java
@@ -1,6 +1,8 @@
 package use_case.analyze_playlist;
 
 public class AnalyzePlaylistInteractor implements AnalyzePlaylistInputBoundary {
+
+    final
     public AnalyzePlaylistInteractor(AnalyzePlaylistDataAccessInterface analyzePlaylistDataAccessObject, AnalyzePlaylistOutputBoundary analyzePlaylistOutputBoundary) {
     }
 
