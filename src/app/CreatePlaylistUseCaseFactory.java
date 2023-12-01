f2b7d89b059cd6e35b26ddde5500ca0a55e373a7
omina26 <omina.nematova@mail.utoronto.ca>
2023-12-01 16:27:20 -0500

    updated CreatePlaylistUseCaseFactory based on updates interactor

--- src/app/CreatePlaylistUseCaseFactory.java
+++ src/app/CreatePlaylistUseCaseFactory.java
@@ -1,33 +1,46 @@
 package app;
 
+import data_access.login.UserDataAccessObject;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.create_playlist.CreatePlaylistController;
 import interface_adapter.create_playlist.CreatePlaylistPresenter;
 import interface_adapter.create_playlist.CreatePlaylistState;
 import interface_adapter.create_playlist.CreatePlaylistViewModel;
 import interface_adapter.playlist_created.PlaylistCreatedViewModel;
+import use_case.create_mood.MoodDataAccessInterface;
 import use_case.create_playlist.CreatePlaylistInputBoundary;
 import use_case.create_playlist.CreatePlaylistInteractor;
 import use_case.create_playlist.CreatePlaylistOutputBoundary;
+import use_case.create_playlist.services.CreatePlaylistAPIHandler;
+import use_case.create_playlist.services.GetRecommendationAPIHandler;
+import use_case.login.LoginDataAccessInterface;
 import view.CreatePlaylistView;
 
 public class CreatePlaylistUseCaseFactory {
     private CreatePlaylistUseCaseFactory() {}
 
     public static CreatePlaylistView create(ViewManagerModel viewManagerModel,
-                                            CreatePlaylistViewModel createPlaylistViewModel, CreatePlaylistState createPlaylistState) {
-        CreatePlaylistController createPlaylistController = createPlaylistUseCase(viewManagerModel, createPlaylistViewModel);
-        return new CreatePlaylistView(createPlaylistViewModel, createPlaylistController, createPlaylistState);
+                                            CreatePlaylistViewModel createPlaylistViewModel,
+                                            PlaylistCreatedViewModel playlistCreatedViewModel,
+                                            LoginDataAccessInterface userDataAccessObject,
+                                            MoodDataAccessInterface moodDataAccessObject) {
+        CreatePlaylistController createPlaylistController = createPlaylistUseCase(viewManagerModel,
+                createPlaylistViewModel, playlistCreatedViewModel, userDataAccessObject, moodDataAccessObject);
+        return new CreatePlaylistView(createPlaylistViewModel, createPlaylistController);
     }
 
     private static CreatePlaylistController createPlaylistUseCase(ViewManagerModel viewManagerModel,
-                                                                  CreatePlaylistViewModel createPlaylistViewModel){
-        //CreatePlaylistOutputBoundary createPlaylistOutputBoundary =
-        //        new CreatePlaylistPresenter(createPlaylistViewModel, playlistCreatedViewModel, viewManagerModel);
+                                                                  CreatePlaylistViewModel createPlaylistViewModel,
+                                                                  PlaylistCreatedViewModel playlistCreatedViewModel,
+                                                                  LoginDataAccessInterface userDataAccessObject,
+                                                                  MoodDataAccessInterface moodDataAccessObject){
+        CreatePlaylistOutputBoundary createPlaylistOutputBoundary =
+                new CreatePlaylistPresenter(createPlaylistViewModel, playlistCreatedViewModel, viewManagerModel);
 
-        //CreatePlaylistInputBoundary createPlaylistInputBoundary = new CreatePlaylistInteractor(createPlaylistOutputBoundary);
-
-        CreatePlaylistInputBoundary createPlaylistInputBoundary = (CreatePlaylistInputBoundary) new CreatePlaylistInteractor();
+        GetRecommendationAPIHandler getRecommendationHandler = new GetRecommendationAPIHandler();
+        CreatePlaylistAPIHandler playlistHandler = new CreatePlaylistAPIHandler();
+        CreatePlaylistInputBoundary createPlaylistInputBoundary = new CreatePlaylistInteractor(userDataAccessObject,
+                moodDataAccessObject, createPlaylistOutputBoundary, getRecommendationHandler, playlistHandler);
 
         return new CreatePlaylistController(createPlaylistInputBoundary);
     }
