9f6b1e8e462160a42daa0db777c7090f5f0620d9
Kelsey Chen <kels.chen@mail.utoronto.ca>
2023-12-02 15:20:33 -0500

    completed TracksAudioFeaturesAPIHandler and TracksAudioFeaturesAPIHandlerInterface

--- src/use_case/login/services/TracksAudioFeaturesAPIHandlerInterface.java
+++ src/use_case/login/services/TracksAudioFeaturesAPIHandlerInterface.java
@@ -1,4 +1,11 @@
 package use_case.login.services;
 
+import javax.json.JsonObject;
+import java.io.IOException;
+import java.util.List;
+
 public interface TracksAudioFeaturesAPIHandlerInterface {
+
+    JsonObject getTracksAudioFeatures(List<String> trackIds, String accessToken) throws IOException, InterruptedException;
+
 }
