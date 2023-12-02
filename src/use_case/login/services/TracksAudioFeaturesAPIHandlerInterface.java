689f1ab699d7450d4ef9bad452bb49c4df864287
Kelsey Chen <kels.chen@mail.utoronto.ca>
2023-12-02 15:20:46 -0500

    added javadoc for TracksAudioFeaturesAPIHandlerInterface

--- src/use_case/login/services/TracksAudioFeaturesAPIHandlerInterface.java
+++ src/use_case/login/services/TracksAudioFeaturesAPIHandlerInterface.java
@@ -5,6 +5,15 @@ import java.io.IOException;
 import java.util.List;
 
 public interface TracksAudioFeaturesAPIHandlerInterface {
+    /**
+     * Retrieves audio features for a list of tracks.
+     *
+     * @param trackIds A list of Spotify track IDs.
+     * @param accessToken Spotify API access token.
+     * @return A JsonObject containing the audio features of the tracks.
+     * @throws IOException If an I/O error occurs.
+     * @throws InterruptedException If the operation is interrupted.
+     */
 
     JsonObject getTracksAudioFeatures(List<String> trackIds, String accessToken) throws IOException, InterruptedException;
 
