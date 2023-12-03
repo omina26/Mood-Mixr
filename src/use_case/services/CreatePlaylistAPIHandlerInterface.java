aa56cc721e47f430b1abb66852f5d86156127296
omina26 <omina.nematova@mail.utoronto.ca>
2023-12-03 11:43:28 -0500

    implemented CreatePlaylistAPIHandler

--- src/use_case/services/CreatePlaylistAPIHandlerInterface.java
+++ src/use_case/services/CreatePlaylistAPIHandlerInterface.java
@@ -1,4 +1,10 @@
 package use_case.services;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
 public interface CreatePlaylistAPIHandlerInterface {
+    public void createPlaylist(String accessToken, String username, String playlistName, String recommendations) throws IOException, InterruptedException;
 }
