414efcd0d29843c2c7085864c9ac70f4684d4788
Kelsey Chen <kels.chen@mail.utoronto.ca>
2023-12-05 04:41:42 -0500

    completed AnalyzePlaylistViewModelTest

--- test/AnalyzePlaylist/AnalyzePlaylistViewModelTest.java
+++ test/AnalyzePlaylist/AnalyzePlaylistViewModelTest.java
@@ -1,4 +1,28 @@
 package AnalyzePlaylist;
 
+import interface_adapter.analyze_playlist.AnalyzePlaylistViewModel;
+import interface_adapter.analyzed_playlist.AnalyzedPlaylistViewModel;
+import interface_adapter.create_mood.CreateMoodViewModel;
+import org.junit.Test;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import static org.junit.Assert.assertEquals;
+
 public class AnalyzePlaylistViewModelTest {
+    @Test
+    public void testAddPropertyChangeListener(){
+        AnalyzePlaylistViewModel viewModel = new AnalyzePlaylistViewModel();
+        PropertyChangeListener listener = new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+
+            }
+        };
+
+        viewModel.addPropertyChangeListener(listener);
+        assertEquals(viewModel.getSupport().getPropertyChangeListeners().length, 1);
+        assertEquals(viewModel.getSupport().getPropertyChangeListeners()[0], listener);
+    }
 }
