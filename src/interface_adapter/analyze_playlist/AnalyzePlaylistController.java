f49752211202c5b7eef28b8a690438d74ef1733e
Kelsey Chen <kels.chen@mail.utoronto.ca>
2023-12-04 19:25:56 -0500

    added javadoc for AnalyzePlaylistController

--- src/interface_adapter/analyze_playlist/AnalyzePlaylistController.java
+++ src/interface_adapter/analyze_playlist/AnalyzePlaylistController.java
@@ -8,12 +8,27 @@ import use_case.analyze_playlist.AnalyzePlaylistInputData;
  */
 
 public class AnalyzePlaylistController {
+
+    /**
+     * Constructs an AnalyzePlaylistController with a given AnalyzePlaylistInputBoundary.
+     *
+     * @param analyzePlaylistInteractor The interactor that provides business logic for
+     *                                  analyzing playlists.
+     */
     final AnalyzePlaylistInputBoundary analyzePlaylistInteractor;
 
     public AnalyzePlaylistController(AnalyzePlaylistInputBoundary analyzePlaylistInteractor) {
         this.analyzePlaylistInteractor = analyzePlaylistInteractor;
 
     }
+
+    /**
+     * Initiates the process of analyzing a playlist.
+     * This method creates an AnalyzePlaylistInputData object and calls the execute method
+     * on the interactor to begin the analysis of the playlist.
+     *
+     * @param playlistID The unique identifier of the playlist to be analyzed.
+     */
 
     public void execute(String playlistID) {
         AnalyzePlaylistInputData data = new AnalyzePlaylistInputData(playlistID);
