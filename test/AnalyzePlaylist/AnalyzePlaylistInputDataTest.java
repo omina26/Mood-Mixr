7a7894be5baee661db8f4126c25439067b7b3102
Kelsey Chen <kels.chen@mail.utoronto.ca>
2023-12-05 02:41:53 -0500

    completed AnalyzePlaylistInputDataTest

--- test/AnalyzePlaylist/AnalyzePlaylistInputDataTest.java
+++ test/AnalyzePlaylist/AnalyzePlaylistInputDataTest.java
@@ -1,4 +1,19 @@
 package AnalyzePlaylist;
 
+import entity.AnalyzedPlaylist;
+import org.junit.Test;
+import use_case.analyze_playlist.AnalyzePlaylistInputData;
+
+import static org.junit.Assert.assertEquals;
+
 public class AnalyzePlaylistInputDataTest {
+    @Test
+    public void TestAnalyzePlaylistInputData(){
+        String playlistID = "mockID";
+
+        AnalyzePlaylistInputData data = new AnalyzePlaylistInputData(playlistID);
+
+        assertEquals(data.toString(), playlistID);
+
+    }
 }
