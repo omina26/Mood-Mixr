54a835376e44021fab1f425380506caf95978f31
Kelsey Chen <kels.chen@mail.utoronto.ca>
2023-12-06 12:06:07 -0500

    fixed errors with group playlist entity

--- src/data_access/group_playlist/GroupPlaylistDataAccessObject.java
+++ src/data_access/group_playlist/GroupPlaylistDataAccessObject.java
@@ -1,13 +1,12 @@
 package data_access.group_playlist;
 
-import entity.Playlist;
 import use_case.group_playlist.GroupPlaylistDataAccessInterface;
 
 import java.util.ArrayList;
 
 public class GroupPlaylistDataAccessObject implements GroupPlaylistDataAccessInterface {
     @Override
-    public ArrayList<Playlist> getPlaylists() {
+    public ArrayList<String> getPlaylists() {
 
         return new ArrayList<>();
     }
