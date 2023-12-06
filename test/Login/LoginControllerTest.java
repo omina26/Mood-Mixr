3485d470d78514a75c5c8b4e9e572b839833757c
Kelsey Chen <kels.chen@mail.utoronto.ca>
2023-12-05 19:32:22 -0500

    completed test for LoginControllerTest

--- test/Login/LoginControllerTest.java
+++ test/Login/LoginControllerTest.java
@@ -1,4 +1,30 @@
 package Login;
 
+import interface_adapter.login.LoginController;
+import org.junit.Test;
+import use_case.login.LoginInputBoundary;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.junit.Assert.assertEquals;
+
 public class LoginControllerTest {
+    @Test
+    public void testExecute() throws IOException, URISyntaxException {
+        MockLoginInteractor mockInteractor = new MockLoginInteractor();
+
+        LoginController controller = new LoginController(mockInteractor);
+        controller.executeUseCase();
+
+        assertEquals(mockInteractor.data, mockInteractor.data);
+    }
+}
+
+class MockLoginInteractor implements LoginInputBoundary{
+    String data;
+    @Override
+    public void execute() {
+        this.data = data;
+    }
 }
