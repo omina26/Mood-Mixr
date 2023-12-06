9f4957833c7aca9b8f09683e8605d5ee5e4aa6ba
Kelsey Chen <kels.chen@mail.utoronto.ca>
2023-12-05 19:54:37 -0500

    completed LoginViewModelTest

--- test/Login/LoginViewModelTest.java
+++ test/Login/LoginViewModelTest.java
@@ -1,4 +1,25 @@
 package Login;
+import interface_adapter.login.LoginViewModel;
+import org.junit.Test;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import static org.junit.Assert.assertEquals;
 
 public class LoginViewModelTest {
+    @Test
+    public void testAddPropertyChangeListener(){
+        LoginViewModel viewModel = new LoginViewModel();
+        PropertyChangeListener listener = new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+
+            }
+        };
+
+        viewModel.addPropertyChangeListener(listener);
+        assertEquals(viewModel.getSupport().getPropertyChangeListeners().length, 1);
+        assertEquals(viewModel.getSupport().getPropertyChangeListeners()[0], listener);
+    }
 }
